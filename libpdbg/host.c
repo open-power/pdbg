d187052b3359e0953aad3e7976e915d80460655e
Joel Stanley <joel@jms.id.au>
2017-09-20 14:35:10 +0930

    Do not redefine _LARGEFILE64_SOURCE
    Signed-off-by: Joel Stanley <joel@jms.id.au>

--- libpdbg/host.c
+++ libpdbg/host.c
@@ -13,7 +13,9 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+#ifndef _LARGEFILE64_SOURCE
 #define _LARGEFILE64_SOURCE
+#endif
 #define _GNU_SOURCE
 #include <stdint.h>
 #include <stdio.h>
