ea071ba66e8f172ba6c6b958d056c0af51b41efb
Amitay Isaacs <amitay@gmail.com>
2018-03-27 11:34:13 +1100

    Makefile: Add rule to generate device tree header file
    Signed-off-by: Amitay Isaacs <amitay@ozlabs.org>

---
+++ generate_dt_header.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+if [ $# -ne 1 ] ; then
+	echo "Usage: $0 <file.dtb>"
+	exit 1
+fi
+
+SYMBOL=$(echo "$1" | tr '.-' '_')
+SYM_START="_binary_${SYMBOL}_o_start"
+SYM_END="_biary_${SYMBOL}_o_end"
+SYM_SIZE="_biary_${SYMBOL}_o_size"
+HEADER="$f.h"
+
+cat - <<EOF
+/* This file is auto-generated from generate_dt_header.sh */
+#ifndef __${SYMBOL}_H__
+#define __${SYMBOL}_H__
+
+unsigned char ${SYM_START};
+unsigned char ${SYM_END};
+long ${SYM_SIZE};
+
+#endif /* __${SYMBOL}_H__ */
+EOF
