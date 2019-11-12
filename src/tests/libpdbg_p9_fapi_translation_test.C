4bb9a39138a6a6ae7f313f878773fa3e9bc38991
Alistair Popple <alistair@popple.id.au>
2019-11-12 13:12:47 +1100

    tests: Add tests to ensure parity with EKB address translations
    The EKB address translation source cannot be imported directly into
    libpdbg as it would require moving the project to C++. So instead the
    translation functions have been manually moved into libpdbg. Obviously
    this introduces the possibility of differences between the two
    implementations.
    
    A test case was added to guard against this possibility by doing a
    comparative test with the original implementation. This involves
    testing every address offset for every target type and possible index.
    
    As there is no simple way of establishing which addresses and indices
    are valid for each target we have to test all of them.
    
    Signed-off-by: Alistair Popple <alistair@popple.id.au>

---
+++ src/tests/libpdbg_p9_fapi_translation_test.C
@@ -0,0 +1,112 @@
+#include <stdio.h>
+#include <inttypes.h>
+
+#define class klass
+#include "libpdbg/libpdbg.h"
+#include "libpdbg/hwunit.h"
+#include "libpdbg/target.h"
+#undef class
+
+#include "p9_scominfo.H"
+
+#include "p9-kernel.dt.h"
+#include "p9.dt.h"
+
+#define MAX_INDEX 30
+
+int test_unit_translation(struct pdbg_target *target, p9ChipUnits_t cu, int index, uint64_t addr)
+{
+	uint64_t pdbg_addr, fapi_addr;
+
+	target->index = index;
+
+	/* TODO: Check standard chiplet translation */
+	if (!target->translate)
+		return 1;
+
+	pdbg_addr = target->translate(target, addr);
+	fapi_addr = p9_scominfo_createChipUnitScomAddr(cu, index, addr, 0);
+
+	if (pdbg_addr != fapi_addr)
+		printf("PDBG Address 0x%016" PRIx64 " does not match FAPI Address 0x%016" PRIx64
+		       " for address 0x%016" PRIx64 " on target %s@%d\n",
+		       pdbg_addr, fapi_addr, addr, pdbg_target_path(target), index);
+
+	return pdbg_addr == fapi_addr;
+}
+
+static struct chip_unit {
+	p9ChipUnits_t cu;
+	const char *classname;
+} chip_unit[] = {
+	{ P9C_CHIP, "" },
+	{ P9N_CHIP, "" },
+	{ P9A_CHIP, "" },
+	{ PU_C_CHIPUNIT, "core" },
+	{ PU_EQ_CHIPUNIT, "eq" },
+	{ PU_EX_CHIPUNIT, "ex" },
+	{ PU_XBUS_CHIPUNIT, "xbus" },
+	{ PU_OBUS_CHIPUNIT, "obus" },
+	{ PU_NV_CHIPUNIT, "nv" },
+	{ PU_PEC_CHIPUNIT, "pec" },
+	{ PU_PHB_CHIPUNIT, "phb" },
+	{ PU_MI_CHIPUNIT, "mi" },
+	{ PU_DMI_CHIPUNIT, "dmi" },
+	{ PU_MCC_CHIPUNIT, "mcc" },
+	{ PU_OMIC_CHIPUNIT, "omic" },
+	{ PU_OMI_CHIPUNIT, "omi" },
+	{ PU_MCS_CHIPUNIT, "mcs" },
+	{ PU_MCA_CHIPUNIT, "mca" },
+	{ PU_MCBIST_CHIPUNIT, "mcbist" },
+	{ PU_PERV_CHIPUNIT, "chiplet" },
+	{ PU_PPE_CHIPUNIT, "ppe" },
+	{ PU_SBE_CHIPUNIT, "sbe" },
+	{ PU_CAPP_CHIPUNIT, "capp" },
+	{ PU_MC_CHIPUNIT, "mc" },
+	{ NONE, NULL },
+};
+
+int main(int argc, const char **argv)
+{
+	struct pdbg_target *target;
+	p9ChipUnits_t cu = NONE;
+	int i, count=0;
+
+	if (argc != 2) {
+		fprintf(stderr, "Usage: %s <class>\n", argv[0]);
+		return 1;
+	}
+
+	// pdbg_set_logfunc(NULL);
+
+	pdbg_targets_init(NULL);
+
+	for (i=0; chip_unit[i].classname; i++) {
+		if (!strcmp(argv[1], chip_unit[i].classname)) {
+			cu = chip_unit[i].cu;
+		}
+	}
+
+	if (cu == NONE) {
+		fprintf(stderr, "Unknown class '%s'\n", argv[1]);
+		return 1;
+	}
+
+	pdbg_for_each_class_target(argv[1], target) {
+		uint64_t addr;
+		int index = pdbg_target_index(target);
+
+		printf("Testing %s  %d\n", pdbg_target_path(target), index);
+		/* Test every sat offset */
+		for (addr = 0; addr < 0xffffffff; addr += 0x40)
+			assert(test_unit_translation(target, cu, index, addr));
+
+		count++;
+	}
+
+	if (count == 0) {
+		printf("Test skipped for class '%s'\n", argv[1]);
+	}
+
+	return 0;
+}
