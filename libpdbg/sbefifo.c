7f470e5e27a1e895e3a6e2dca1859555605514c7
Amitay Isaacs <amitay@ozlabs.org>
2020-11-11 15:34:05 +1100

    libpdbg: Add dump() method to chipop target
    Signed-off-by: Amitay Isaacs <amitay@ozlabs.org>

--- libpdbg/sbefifo.c
+++ libpdbg/sbefifo.c
@@ -205,6 +205,14 @@ static int sbefifo_op_mpipl_get_ti_info(struct chipop *chipop, uint8_t **data, u
 	return sbefifo_mpipl_get_ti_info(sctx, data, data_len);
 }
 
+static int sbefifo_op_dump(struct chipop *chipop, uint8_t type, uint8_t clock, uint8_t **data, uint32_t *data_len)
+{
+	struct sbefifo *sbefifo = target_to_sbefifo(chipop->target.parent);
+	struct sbefifo_context *sctx = sbefifo->get_sbefifo_context(sbefifo);
+
+	return sbefifo_get_dump(sctx, type, clock, data, data_len);
+}
+
 static struct sbefifo *pib_to_sbefifo(struct pdbg_target *pib)
 {
 	struct pdbg_target *target;
@@ -800,6 +808,7 @@ static struct chipop sbefifo_chipop = {
 	.mpipl_enter = sbefifo_op_mpipl_enter,
 	.mpipl_continue = sbefifo_op_mpipl_continue,
 	.mpipl_get_ti_info = sbefifo_op_mpipl_get_ti_info,
+	.dump = sbefifo_op_dump,
 };
 DECLARE_HW_UNIT(sbefifo_chipop);
 
